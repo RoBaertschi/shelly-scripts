9d169d01eb9adb612e6145610d18d4f6164e840c
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-02-12 18:27:27 +0100

    nixified this

---
+++ flake.nix
@@ -0,0 +1,43 @@
+# ml2 sts=2 ts=2 ml2
+
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+    version = builtins.substring 0 8 lastModifiedDate;
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system; overlays = [ self.overlay ];});
+  in {
+    overlay = final: prev: {
+      shelly = with final; stdenv.mkDerivation {
+        name = "shelly-${version}";
+        unpackPhase = ":";
+        buildPhase = ":";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${./tmux-sessionizer} $out/bin/tmux-sessionizer
+          cp ${./tmux-sessionizer-clone} $out/bin/tmux-sessionizer-clone
+          cp ${./rfv} $out/bin/rfv
+          '';
+      };
+    };
+    packages = forAllSystems(system: {
+      inherit (nixpkgsFor.${system}) shelly;
+    });
+    nixosModules.shelly = {pkgs, ...}:
+      {
+          nixpkgs.overlays = [self.overlay];
+          environment.systemPackages = [ pkgs.shelly pkgs.fzf pkgs.tmux pkgs.ripgrep pkgs.bat pkgs.gh pkgs.jq ];
+      };
+    defaultPackage = forAllSystems (system: self.packages.${system}.shelly);
+  };
+}
